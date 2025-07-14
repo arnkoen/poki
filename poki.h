840ea810db719a9e1a52a4852936e71ed7e9cf01
Arne Koenig <82873244+arnkoen@users.noreply.github.com>
2025-07-14 17:21:57 +0200

    Update poki.h

--- poki.h
+++ poki.h
@@ -1,12 +1,12 @@
-#pragma once
-
 /*
 Poki - minimal creative coding framework
 
-Copyright (c) Arne Koenig 2025
+Copyright (c) 2025, Arne Koenig
 Redistribution and use in source and binary forms, with or without modification, are permitted.
 THIS SOFTWARE IS PROVIDED 'AS-IS', WITHOUT ANY EXPRESS OR IMPLIED WARRANTY. IN NO EVENT WILL THE AUTHORS BE HELD LIABLE FOR ANY DAMAGES ARISING FROM THE USE OF THIS SOFTWARE.
 */
+
+#pragma once
 
 #include "deps/sokol_gfx.h"
 #include "deps/sokol_fetch.h"
